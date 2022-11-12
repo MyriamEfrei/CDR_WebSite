062787dec4c795d34edea2b85425405d4b0076d8
Serquand <=>
2022-11-12 06:29:06 +0100

    Refactor

--- src/Components/France map/FranceMap.jsx
+++ src/Components/France map/FranceMap.jsx
@@ -1,15 +1,12 @@
-import Region from "./Region"
 import Informations from './FranceMapInfo.json';
 
 const FranceMap = () => {
     const regions = Informations.allRegions; 
     console.log(regions[0].path);
     return (
-        <svg className="france-map" xmlns="http://www.w3.org/2000/svg" width="300" height="325" viewBox="0 0 597.6 670.7" overflow="visible">
-            {regions.map(region => <Region 
-                pathMaking={ region.path } 
-                key={ region.name }
-            />
+        <svg className="france-map" xmlns="http://www.w3.org/2000/svg" width="600" height="505" viewBox="0 0 597.6 670.7" overflow="visible">
+            {regions.map(region => 
+                <path key={ region.name } fill="#eee" stroke="#000" d={ region.path }></path>
             )}
         </svg>
     )
