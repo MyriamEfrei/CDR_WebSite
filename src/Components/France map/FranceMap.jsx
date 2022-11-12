d701d2f45224810b6abcfccc8dcdc1078aab4c92
Serquand <=>
2022-11-12 06:40:49 +0100

    Add fill color on hover

--- src/Components/France map/FranceMap.jsx
+++ src/Components/France map/FranceMap.jsx
@@ -1,12 +1,17 @@
 import Informations from './FranceMapInfo.json';
+import "./FranceMap.css"
 
 const FranceMap = () => {
     const regions = Informations.allRegions; 
-    console.log(regions[0].path);
+
     return (
         <svg className="france-map" xmlns="http://www.w3.org/2000/svg" width="600" height="505" viewBox="0 0 597.6 670.7" overflow="visible">
             {regions.map(region => 
-                <path key={ region.name } fill="#eee" stroke="#000" d={ region.path }></path>
+                <path 
+                    key={ region.name } 
+                    stroke="#000"
+                    d={ region.path }
+                ></path>
             )}
         </svg>
     )
